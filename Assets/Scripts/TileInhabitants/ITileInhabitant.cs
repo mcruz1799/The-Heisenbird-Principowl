32a07627c9cc6c9fb23f88e266d6d11383e28275
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-27 16:48:07 -0500

    Add SetPosition to ITileInhabitant

--- Assets/Scripts/TileInhabitants/ITileInhabitant.cs
+++ Assets/Scripts/TileInhabitants/ITileInhabitant.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface ITileInhabitant {
+  void SetPosition(int newRow, int newCol, out bool success);
+
   //Returns a set containing the Tiles occupied by this
   ISet<Tile> Occupies();
 
