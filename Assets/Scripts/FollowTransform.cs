b6d4b21d2e22c6e779c5c379dc9353b3ecb11230
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-26 21:34:12 -0500

    Replace CameraFollow with FollowTransform

--- Assets/Scripts/FollowTransform.cs
+++ Assets/Scripts/FollowTransform.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class FollowTransform : MonoBehaviour {
 #pragma warning disable 0649
   [SerializeField] private Transform toFollow;
   [SerializeField] private Vector3 localOffset;
   [SerializeField] private bool useInspectorOffset;
 
   [SerializeField] private bool smoothMotion;
-  [SerializeField] private float cameraDist;
   [Range(1f, 100f)] [SerializeField] private float smoothMotionSpeed;
 
   [SerializeField] private bool lockX;
@@ -29,17 +28,20 @@ public class CameraFollow : MonoBehaviour {
 
     if (smoothMotion) {
       Vector3 selfToTarget = newPosition - oldPosition;
-      if (selfToTarget.sqrMagnitude <= cameraDist) { //distance bw camera and obj
+      if (selfToTarget.sqrMagnitude <= 0.04f) { //distance bw camera and obj
         transform.position = newPosition;
       } else {
-        transform.position = Vector3.MoveTowards(oldPosition, newPosition, smoothMotionSpeed*Time.deltaTime);
+        //transform.Translate(selfToTarget.normalized * Time.deltaTime * smoothMotionSpeed);
+        //Vector3 velocity = new Vector3(smoothMotionSpeed, 0, smoothMotionSpeed);
+        //transform.position = Vector3.SmoothDamp(oldPosition, newPosition, ref velocity, 1f);
+        transform.position = Vector3.MoveTowards(oldPosition, newPosition, smoothMotionSpeed * Time.deltaTime);
       }
     } else {
       transform.position = newPosition;
     }
     Vector3 finalPosition = transform.position;
     if (lockX) {
-       finalPosition.x = oldPosition.x;
+      finalPosition.x = oldPosition.x;
     }
     if (lockY) {
       finalPosition.y = oldPosition.y;
