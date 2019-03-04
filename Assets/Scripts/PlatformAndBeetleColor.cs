eaa0be07e4840e056d3b020915dd415b62837bee
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-04 15:41:58 -0500

    Add enum for platform-beetle color groups

---
+++ Assets/Scripts/PlatformAndBeetleColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlatformAndBeetleColor { None, Cyan }
+
+public static class PlatformAndBeetleColorExtensions {
+  public static Color32 RgbColor(this PlatformAndBeetleColor color) {
+    switch (color) {
+      case PlatformAndBeetleColor.Cyan:
+        return Color.cyan;
+
+      default:
+        throw new System.ArgumentException("Illegal enum value");
+    }
+  }
+}
