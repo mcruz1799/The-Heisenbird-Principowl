bee17e61db34d94b9ea0da49c294c531b0cc9812
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-05 17:57:17 -0500

    Comment out unused extension class

--- Assets/Scripts/PlatformAndBeetleColor.cs
+++ Assets/Scripts/PlatformAndBeetleColor.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public enum PlatformAndBeetleColor { None, Cyan }
 
-public static class PlatformAndBeetleColorExtensions {
-  public static Color32 RgbColor(this PlatformAndBeetleColor color) {
-    switch (color) {
-      case PlatformAndBeetleColor.Cyan:
-        return Color.cyan;
+//public static class PlatformAndBeetleColorExtensions {
+//  public static Color32 RgbColor(this PlatformAndBeetleColor color) {
+//    switch (color) {
+//      case PlatformAndBeetleColor.Cyan:
+//        return Color.cyan;
 
-      default:
-        throw new System.ArgumentException("Illegal enum value");
-    }
-  }
-}
+//      default:
+//        throw new System.ArgumentException("Illegal enum value");
+//    }
+//  }
+//}
