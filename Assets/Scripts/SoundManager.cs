e28723ad3be0163030dee0d8b6f34eb38835d1ca
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-15 14:18:55 -0400

    Address warnings

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
   public static SoundManager S { get; private set; }
-  private AudioSource audio;
+  private AudioSource audioSource;
   public AudioClip HeadBonk;
   public AudioClip Landed;
   public AudioClip Jump;
@@ -24,12 +24,12 @@ public class SoundManager : MonoBehaviour {
         Destroy (gameObject);
     //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
     DontDestroyOnLoad (gameObject);
-    audio = GetComponent<AudioSource>();
+    audioSource = GetComponent<AudioSource>();
   }
 
   public void PlayerHeadBonk() {
-    audio.clip = HeadBonk;
-    audio.Play();
+    audioSource.clip = HeadBonk;
+    audioSource.Play();
   }
 
   public void PlayerLanded() {
@@ -38,21 +38,21 @@ public class SoundManager : MonoBehaviour {
   }
 
   public void PlayerJump() {
-    audio.clip = Jump;
-    audio.Play();
+    audioSource.clip = Jump;
+    audioSource.Play();
   }
 
   public void PlayerDamaged() {
-    audio.clip = Damaged;
-    audio.Play();
+    audioSource.clip = Damaged;
+    audioSource.Play();
   }
 
   public void PlayerDied() {
-    audio.clip = PlayerDeath;
-    audio.Play();
+    audioSource.clip = PlayerDeath;
+    audioSource.Play();
   }
   public void BeetleDied(){
-    audio.clip = BeetleDeath;
-    audio.Play();
+    audioSource.clip = BeetleDeath;
+    audioSource.Play();
   }
 }
