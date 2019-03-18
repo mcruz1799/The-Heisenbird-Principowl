af803abf7fd7739484fa6c7a867922f4ce3eace4
Kalpa <kanjur@andrew.cmu.edu>
2019-03-18 18:56:59 -0400

    updated follower enemy script

--- Assets/Scripts/TileInhabitants/Characters/FollowerEnemyObject.cs
+++ Assets/Scripts/TileInhabitants/Characters/FollowerEnemyObject.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public class FollowerEnemyObject : EnemyObject
 {
-  [Range(1, 10)] public int maxDistFromHome;
-  [Range(1, 10)] public int playerFollowDistance;
+  [Range(1, 10)] public int attackRange;
 }
