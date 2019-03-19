69eee7eca0ff22bc8491564802f15f758088ef2b
Kalpa <kanjur@andrew.cmu.edu>
2019-03-18 21:35:06 -0400

    attacks follower

---
+++ Assets/Scripts/LevelGeneration/UpdraftTileMaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdraftTileMaker : TileInhabitantMaker
+{
+#pragma warning disable 0649
+  [SerializeField] private UpdraftTileObject updraftTilePrefab;
+#pragma warning restore 0649
+
+  public override ITileInhabitant Make(int row, int col, Transform parent = null) {
+    return UpdraftTile.Make(updraftTilePrefab, row, col, parent);
+  }
+}
---
+++ Assets/Scripts/TileInhabitants/FlammableTile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlammableTile : SingleTileEntity, ITurnTaker
+{
+
+  private readonly FlammableTileObject gameObject;
+  private UpdraftTileMaker updraftTileMaker;
+  private bool isOnFire;
+
+  public FlammableTile(FlammableTileObject gameObject) : base(gameObject) {
+    this.gameObject = gameObject;
+    this.isOnFire = false;
+    this.updraftTileMaker = gameObject.updraftTileMaker;
+    GameManager.S.RegisterTurnTaker(this);
+    SetPosition(gameObject.spawnRow, gameObject.spawnCol, out bool success);
+    if (!success) {
+      throw new System.Exception("Failed to initialize FlammableTile");
+    }
+  }
+
+  protected override bool IsBlockedByCore(ITileInhabitant other){
+    if (other is Platform) {
+      Platform platform = (Platform)other;
+      return platform.IsActive;
+    }
+    return false;
+  }
+
+  public void OnTurn() {
+    if (!isOnFire){
+      IReadOnlyCollection<ITileInhabitant> inhabitants = GameManager.S.Board[Row, Col].Inhabitants;
+      foreach (ITileInhabitant habiter in inhabitants){
+        if (habiter is FollowerEnemy){
+          isOnFire = true;
+          habiter.TakeDamage(1);
+          for (int i = 1; i <= gameObject.numUpdraftTiles; i++){
+            updraftTileMaker.Make(Row + i, Col, null);
+          }
+        }
+      }
+    }
+  }
+
+  public static FlammableTile Make(FlammableTileObject flammableTilePrefab, int row, int col, Transform parent = null) {
+    flammableTilePrefab = Object.Instantiate(flammableTilePrefab);
+    flammableTilePrefab.transform.parent = parent;
+    flammableTilePrefab.spawnRow = row;
+    flammableTilePrefab.spawnCol = col;
+    return new FlammableTile(flammableTilePrefab);
+  }
+}
