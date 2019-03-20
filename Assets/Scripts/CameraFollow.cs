4e0ef6cd463045f21b260b83f581b37a071aa5bd
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-20 09:02:44 -0400

    Style fixes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
 #pragma warning disable 0649
   [SerializeField] private Transform toFollow;
   [SerializeField] private Vector3 localOffset;
@@ -12,8 +11,8 @@ public class CameraFollow : MonoBehaviour
   [SerializeField] private bool smoothMotion;
   [Range(1f, 100f)] [SerializeField] private float smoothMotionSpeed = 4f;
 
-  [SerializeField] float xThreshold;
-  [SerializeField] float yThreshold;
+  [SerializeField] private float xThreshold = 0;
+  [SerializeField] private float yThreshold = 3;
 
   [SerializeField] private bool lockX;
   [SerializeField] private bool lockY;
@@ -24,34 +23,26 @@ public class CameraFollow : MonoBehaviour
   private float yDifference;
   private Vector3 moveTemp;
 
-  private void Awake()
-  {
+  private void Awake() {
     if (!useInspectorOffset) {
       localOffset = transform.position - toFollow.position;
     }
-    xThreshold = 0;
-    yThreshold = 3;
   }
 
-  private void LateUpdate()
-  {
+  private void LateUpdate() {
     Vector3 currentPosition = transform.position;
     Vector3 targetPosition = toFollow.position + localOffset;
 
     Vector3 finalPosition;
-
     finalPosition = targetPosition;
 
-    // inside your Update function
     if (smoothMotion) {
-
- 
       xDifference = Mathf.Abs(targetPosition.x - currentPosition.x);
       yDifference = Mathf.Abs(targetPosition.y - currentPosition.y);
 
-    if (xDifference >= xThreshold || yDifference >= yThreshold) {
-      finalPosition = Vector3.Lerp(currentPosition, targetPosition, smoothMotionSpeed * Time.deltaTime);
-    }
+      if (xDifference >= xThreshold || yDifference >= yThreshold) {
+        finalPosition = Vector3.Lerp(currentPosition, targetPosition, smoothMotionSpeed * Time.deltaTime);
+      }
       finalPosition.x = (xDifference < xThreshold) ? currentPosition.x : finalPosition.x;
       finalPosition.y = (yDifference < yThreshold) ? currentPosition.y : finalPosition.y;
 
