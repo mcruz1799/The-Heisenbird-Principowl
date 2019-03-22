203664f485caa1b50cee40da685abea606e87063
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-21 22:24:52 -0400

    Update PlayerAnimator

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,60 +5,46 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour {
 #pragma warning disable 0649
   [SerializeField] private SpriteAnimator idleGrounded;
-  [SerializeField] private SpriteAnimator jumping;
+  [SerializeField] private SpriteAnimator midair;
   [SerializeField] private SpriteAnimator running;
-  private SpriteAnimator current_animator = null;
-
-  //[SerializeField] private ISpriteAnimator midair;
 #pragma warning restore 0649
+
+  private readonly List<ISpriteAnimator> animators = new List<ISpriteAnimator>();
 
   private Player Player => GameManager.S.Player;
 
+  private void Awake() {
+    animators.Add(idleGrounded);
+    animators.Add(midair);
+    animators.Add(running);
+  }
+
   private void Update() {
-    //Flip PlayerGraphic Orientation
+    animators.ForEach(a => DisableAnimator(a));
+    animators.ForEach(a => a.FlipX = Player.XVelocity > 0 ? false : true);
+    animators.ForEach(a => a.FlipY = Player.IsStunned);
+
     if (Player.YVelocity != 0) {
-      if(!jumping.gameObject.activeSelf){
-        disableAllAnimators();
-        jumping.IsVisible = true;
-        jumping.IsPaused = false;
-        jumping.StartFromFirstFrame();
-        current_animator = jumping;
-      }
-      this.flipPlayerX(jumping);
+      EnableAnimator(midair);
+
+    } else if (Player.XVelocity != 0) {
+      EnableAnimator(running);
+
+    } else{
+      EnableAnimator(idleGrounded);
     }
-    else if (Player.XVelocity != 0){
-      if(!running.gameObject.activeSelf){
-        disableAllAnimators();
-        running.IsVisible = true;
-        running.IsPaused = false;
-        running.StartFromFirstFrame();
-        current_animator = running;
-      }
-      this.flipPlayerX(running);
-    }
-    else{
-      if(!idleGrounded.gameObject.activeSelf){
-        disableAllAnimators();
-        idleGrounded.IsVisible = true;
-        idleGrounded.IsPaused = false;
-        idleGrounded.StartFromFirstFrame();
-        current_animator = idleGrounded;
-      }
-      this.flipPlayerX(idleGrounded);
-    }
-    if(current_animator) current_animator.FlipY = Player.IsStunned;
   }
 
-  private void disableAllAnimators(){
-    idleGrounded.IsPaused = false;
-    idleGrounded.IsVisible = false;
-    jumping.IsPaused = false;
-    jumping.IsVisible = false;
-    running.IsPaused = false;
-    running.IsVisible = false;
+  private void DisableAnimator(ISpriteAnimator animator) {
+    animator.IsPaused = true;
+    animator.IsVisible = false;
   }
 
-  private void flipPlayerX(SpriteAnimator s){
-    s.FlipX = Player.XVelocity >= 0 ? false : true;
+  private void EnableAnimator(ISpriteAnimator animator) {
+    if (!animator.IsVisible) {
+      animator.IsPaused = false;
+      animator.IsVisible = true;
+      animator.StartFromFirstFrame();
+    }
   }
 }
