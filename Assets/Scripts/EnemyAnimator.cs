ef71cbf13c505213fc7a7014b1bf5b8d4a87fa91
Kalpa <kanjur@andrew.cmu.edu>
2019-03-26 16:13:58 -0400

    working on boss, enemy animator

---
+++ Assets/Scripts/EnemyAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour {
+#pragma warning disable 0649
+  [SerializeField] private SpriteAnimator idle;
+  private IEnemy enemy;
+
+#pragma warning restore 0649
+  private void Awake(){
+    idle.StartFromFirstFrame();
+    enemy = transform.parent.GetComponent<IEnemy>();
+  }
+
+  private void LateUpdate() {
+    if (enemy.XVelocity > 0) {
+      idle.FlipX = false;
+
+    } else if (enemy.XVelocity < 0) {
+      idle.FlipX = true;
+
+    }
+  }
+}
