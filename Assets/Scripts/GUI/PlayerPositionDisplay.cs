f221b5fa3eb09cda9b68e2ef8c7373d021622679
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-03-28 09:43:46 -0400

    Display player's xy-position on-screen (for debugging)

---
+++ Assets/Scripts/GUI/PlayerPositionDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPositionDisplay : MonoBehaviour {
+  private Text text;
+  private void Awake() {
+    text = GetComponent<Text>();
+  }
+
+  private void Update() {
+    text.text = string.Format("(x, y) = ({0}, {1})", GameManager.S.Player.Col, GameManager.S.Player.Row);
+  }
+}
